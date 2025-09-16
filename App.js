661323063219ba8a291d9c89514ff6c269a82cce
NajeebSayyed <najeebsayyed777@gmail.com>
2025-09-16 15:09:29 +0530

    Project Setup

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Navigation from './src/navigation/Navigation';
 import './global.css';
-import { Text, View } from 'react-native';
-import CustomText from './src/components/CustomText';
-import PrimaryButton from './src/components/PrimaryButton';
 const App = () => {
-  return <Navigation></Navigation>;
+  return <Navigation />;
 };
 
 export default App;
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,7 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from '../screens/LoginScreen';
-import SignupScreen from '../screens/SignupScreen';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
@@ -13,8 +11,6 @@ const Navigation = () => {
         initialRouteName="HomeScreen"
         screenOptions={{ headerShown: false }}
       >
-        {/* <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="SignupScreen" component={SignupScreen} /> */}
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
