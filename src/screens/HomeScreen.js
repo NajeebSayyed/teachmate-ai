52005ccaaf51fddc0a1fa0179414a8fdbfd079c5
NajeebSayyed <najeebsayyed777@gmail.com>
2025-09-25 14:25:17 +0530

    HomeScreen Completed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
+import CustomText from '../components/CustomText';
+import CustomButton from '../components/CustomButton';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-mintCream items-center ">
+      <CustomText
+        text="🎯 AI-Powered Quizzes"
+        textStyle={{
+          marginTop: '45%',
+        }}
+      />
+      <CustomText text="⚡ Difficulty Control" />
+      <CustomText text="📑 Export Tests " />
+      <CustomText
+        text="Generate your quiz!"
+        textStyle={{
+          marginTop: 30,
+          backgroundColor: '#E7F2EF',
+          color: '#4E616A',
+        }}
+      />
+      <CustomButton
+        title="Lets Start !"
+        className="absolute bottom-60 self-center"
+        textStyle={{ fontFamily: 'Nunito-Bold', fontSize: 35 }}
+        // onPress={() => navigation.navigate('GenerateScreen')}
+      />
     </View>
   );
 };
